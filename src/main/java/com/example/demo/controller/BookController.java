e3b3cad43ca69e64d154d9bd09745888c65e3e27
André Picoli <picoli.andre@gmail.com>
2018-11-18 16:51:52 -0200

    Inclusão de telas.

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,16 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
-
-	@GetMapping
+/*
+	@GetMapping("/books")
 	public String getName() {
 		return "Book name 1";
+	}*/
+	
+	@RequestMapping("/")
+	public String login(Map<String, Object> model) {
+		return "login";
+	}
+	
+	@RequestMapping("/")
+	public String dashboard(Map<String, Object> model) {
+		return "dashboard";
 	}
 	
 }
